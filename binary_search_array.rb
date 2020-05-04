d91ebfcedaaac5aa321885145a1a72e79cd1f7a6
Ali Andersen <alireza.andersen@gmail.com>
2020-05-04 15:44:18 -0500

    binary_search_array

---
+++ binary_search_array.rb
@@ -0,0 +1,33 @@
+def solution(arr)
+	
+end
+
+arr1 = [1,4,100, 5]
+response1 = "Right"
+ar1 = solution(arr1)
+puts "ARR1:#{ar1}:::#{ar1 == response1}"
+
+arr2 = [1, 10, 5, 1, 0, 6]
+response2 = ""
+ar2 = solution(arr2)
+puts "ARR2:#{ar2}:::#{ar2 == response2}"
+
+arr3 = []
+response3 = ""
+ar3 = solution(arr3)
+puts "ARR3:#{ar3}:::#{ar3 == response3}"
+
+arr4 = [1]
+response4 = ""
+ar4 = solution(arr4)
+puts "ARR4:#{arr4}:::#{ar4 == response4}"
+
+arr5 = [3, 6, 2, 9, -1, 10]
+response5 = "Left"
+ar5 = solution(arr5)
+puts "ARR5:#{ar5}:::#{ar5 == response5}"
+
+# Write a fucntion that determines whether the left or right branch of the tree is larger.
+# The size of each branch is the sum of the node values. The function should return The
+# string "Right" if the right side is larger and "Left" if the left side is larger. If the
+# tree has 0 nodes or if the size of the branches are equal, return an empty string.
