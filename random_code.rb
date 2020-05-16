d2cc9bd494b42d8534ddff73b4a7dddeb9dc58ba
Ali Andersen <alireza.andersen@gmail.com>
2020-05-15 23:52:59 -0500

    random code

--- random_code.rb
+++ random_code.rb
@@ -24,6 +24,15 @@ def convert_time(seconds)
   Time.at(seconds).utc.strftime("%l:%M:%S")
 end
 
+# Given a string S that only contains "I" (increase) or "D" (decrease), let N = S.length.
+# Return any permutation A of [0, 1, ..., N] such that for all i = 0, ..., N-1:
+#   If S[i] == "I", then A[i] < A[i+1]
+#   If S[i] == "D", then A[i] > A[i+1]
+#
+# Example 1:
+# Input: "IDID"
+# Output: [0,4,1,3,2]
+
 def di_string_match(s)
   min = 0
   max = s.length
