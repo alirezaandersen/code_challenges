4069798523106244de790b391c9b77a3026b3a60
Ali Andersen <alireza.andersen@gmail.com>
2020-07-06 21:40:03 -0700

    acronym_conveter

---
+++ acronym_converter.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+# The problem is to implement a function that converts a given phrase to its
+# acronym. The function should accept the phrase as input and print or return
+# the acronym as its output. Your code should be compilable/runnable.
+
+# Examples:
+#  - Portable Network Graphics => PNG
+#  - First In, First Out => FIFO
+#  - Asynchronous Javascript and XML => AJAX
+#  - GNU Image Manipulation Program => GIMP
+#  - Complementary metal-oxide semiconductor => CMOS
+#  - Something - I made up from thin air => SIMUFTA
