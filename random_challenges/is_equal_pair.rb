a6634c55820c5ad68aad469b2107e59452f616d3
Alireza Andersen <alireza.andersen@gmail.com>
2022-02-23 18:00:02 -0600

    edge cases met

--- random_challenges/is_equal_pair.rb
+++ random_challenges/is_equal_pair.rb
@@ -16,72 +16,87 @@ class DebugUtil
       l_obj.is_a?(Hash) && r_obj.is_a?(Hash)
       self.hash_compare(l_obj, r_obj)
     else
-    compare_primitive(l_obj, r_obj)
+      l_obj.to_s == r_obj.to_s
     end
-    # your code here
   end
 
-  def self.compare_primitive(l_obj, r_obj)
-    n1 = l_obj.to_s
-    n2 = r_obj.to_s
-    n1 ==(n2)
+  def self.arr_compare(arr_1, arr_2)
+    return false unless arr_1.size === arr_2.size
+    ar1 = self.type_of_array(arr_1)
+    ar2 = self.type_of_array(arr_2)
+
+    return false unless ar1 == ar2
+    true
   end
 
-    def self.arr_compare(arr_1, arr_2)
-    # based on size
-     return false unless arr_1.size === arr_2.size
-     return false unless arr_1.map(&:to_s).sort == arr_2.map(&:to_s).sort
-       true
+  def self.type_of_array(arr)
+    arr.map do |e|
+      if e.is_a?(Hash)
+        e.map { |k, v| { k.to_s => self.parse_type(v)} }
+      elsif e.is_a?(Array) && e.size > 1
+        e.flatten.map(&:to_s)
+      else
+        e.to_s
+      end
     end
+  end
 
-    def self.hash_compare(hash_1, hash_2)
-      binding.pry
-      # return false unless hash_1 === hash_2
-      arr = []
-      arr << (hash_1.keys | hash_2.keys).map { |key| self.compare_primitive(hash_1[key], hash_2[key]) }
-      arr << (hash_1.values | hash_2.values).map { |val| self.compare_primitive(hash_1[val], hash_2[val]) }
-      arr.flatten!
-      arr.include?(true)
-    end
+  def self.hash_compare(hash_1, hash_2)
+    h2 = hash_2.map { |k, v| { k.to_s => self.parse_type(v)} }
+    h1 = hash_1.map { |k, v| { k.to_s => self.parse_type(v)} }
+    return false unless self.sorted_hash(h1, h2)
+    true
+  end
+
+  def self.parse_type(v)
+    return v.to_s unless v.is_a?(Array)
+    v.map(&:to_s)
+  end
+
+  def self.sorted_hash(h1,h2)
+    s1 = h1.sort_by! { |k| k.keys }
+    s2 = h2.sort_by! { |k| k.keys }
+    s1 == s2
+  end
 end
 
 describe "DebugUtil" do
   context "base cases" do
-    # it "3 == 3" do
-    #   expect(DebugUtil._is_object_pair_equal?(3,3)).to eq true
-    # end
-    #
-    # it ":foo == 'foo'" do
-    #   expect(DebugUtil._is_object_pair_equal?(:foo, "foo")).to eq true
-    # end
-    #
-    # it "'foo' == 'foo'" do
-    #   expect(DebugUtil._is_object_pair_equal?("foo","foo")).to eq true
-    # end
-    #
-    # it "3 == '3'" do
-    #   expect(DebugUtil._is_object_pair_equal?(3,"3")).to eq true
-    # end
-    #
-    # it "[1] == [1]" do
-    #   expect(DebugUtil._is_object_pair_equal?([1],[1])).to eq true
-    # end
-    #
-    # it "[1,2] == ['1','2']" do
-    #   expect(DebugUtil._is_object_pair_equal?([1,2],["1","2"])).to eq true
-    # end
-    #
-    # it "[1,2,3] != [1,2]" do
-    #   expect(DebugUtil._is_object_pair_equal?([1,2,3],[1,2])).to eq false
-    # end
-    #
-    # it "[1,2] != [1,2,3,4]" do
-    #   expect(DebugUtil._is_object_pair_equal?([1,2],[1,2,3,4])).to eq false
-    # end
-    #
-    # it "hash simple equals another hash" do
-    #   expect(DebugUtil._is_object_pair_equal?({foo: "bar"}, {foo: "bar"})).to eq true
-    # end
+    it "3 == 3" do
+      expect(DebugUtil._is_object_pair_equal?(3,3)).to eq true
+    end
+
+    it ":foo == 'foo'" do
+      expect(DebugUtil._is_object_pair_equal?(:foo, "foo")).to eq true
+    end
+
+    it "'foo' == 'foo'" do
+      expect(DebugUtil._is_object_pair_equal?("foo","foo")).to eq true
+    end
+
+    it "3 == '3'" do
+      expect(DebugUtil._is_object_pair_equal?(3,"3")).to eq true
+    end
+
+    it "[1] == [1]" do
+      expect(DebugUtil._is_object_pair_equal?([1],[1])).to eq true
+    end
+
+    it "[1,2] == ['1','2']" do
+      expect(DebugUtil._is_object_pair_equal?([1,2],["1","2"])).to eq true
+    end
+
+    it "[1,2,3] != [1,2]" do
+      expect(DebugUtil._is_object_pair_equal?([1,2,3],[1,2])).to eq false
+    end
+
+    it "[1,2] != [1,2,3,4]" do
+      expect(DebugUtil._is_object_pair_equal?([1,2],[1,2,3,4])).to eq false
+    end
+
+    it "hash simple equals another hash" do
+      expect(DebugUtil._is_object_pair_equal?({foo: "bar"}, {foo: "bar"})).to eq true
+    end
 
     it "hash doesn't equal hash of different" do
       expect(DebugUtil._is_object_pair_equal?({}, {a: nil})).to eq false
