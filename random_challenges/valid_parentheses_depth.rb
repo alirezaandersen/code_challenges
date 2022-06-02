53ee891993266933ace14a745dc434a003abca11
Alireza Andersen <alireza.andersen@gmail.com>
2022-06-02 17:07:33 -0500

    valid_parentheses_depth.rb

--- random_challenges/valid_parentheses_depth.rb
+++ random_challenges/valid_parentheses_depth.rb
@@ -9,18 +9,22 @@ def valid_parentheses(str)
   str.each_char do |c|
     stack << c if symbols.key?(c)
     return false if symbols.key(c) && (symbols.key(c) != stack.pop)
-    stack.size > stack_size ? stack_size += 1 : nil # want to keep track of only largest parenthese set
+    stack.size > stack_size ? stack_size += 1 : nil # want to keep track of only largest parentheses set
   end
 
   stack_size
 
 end
 
-
+=begin
 str = "((())) (())"
 #returns 3 -> 3, 2
 puts valid_parentheses(str)
 
 str = "((())) (((((())))))"
 #returns 6 -> 3, 6
+puts valid_parentheses(str)
+=end
+str = "() (("
+#returns 1 -> 1, 0 -invalid parentheses
 puts valid_parentheses(str)
